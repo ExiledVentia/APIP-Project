f952c4bdfc6077ef4cf76eb27374a869e27c44f9
Dakrezzz <daffaatharizq20@gmail.com>
2024-10-15 16:27:29 +0800

    <update kode dari tailwind.js>

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>User Index</title>
 </head>
 
@@ -41,7 +42,7 @@
                                 @method('DELETE')
                                 <button type="submit">Delete</button>
 
-
+                                
                                 
                             </form>
                         </td>
