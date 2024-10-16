d7f47e5765b540e70cb382ae41556cd8595eef76
Dakrezzz <daffaatharizq20@gmail.com>
2024-10-16 18:12:35 +0800

    pembaruan create

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -6,28 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ADD USER DATA</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <!-- HTML form for adding user data. --> 
-<body>
+<body class="bg-gray-800">
     <center>
-        <h1>ADD USER DATA</h1>
+        <h1 style="color:white;">ADD USER DATA</h1>
     </center>
     <form action="{{route('user.store')}}" method="POST">
-        @csrf
-        <label for="nama">Nama:</label>
-        <input type="text" name="nama" required><br>
-
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br>
-
-        <label for="password">password:</label>
-        <input type="password" name="password" required><br>
-
-        <label for="email">Email:</label>
-        <textarea name="email" required></textarea><br>
-
-        <button type="submit">Submit</button>
-    </form>
+        @csrf  
+        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div class="sm:col-span-2">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your name" required="">
+            </div>
+            <div class="w-full">
+                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Username" required="">
+            </div>
+            <div class="w-full">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Password" required="">
+            </div>
+            <div class="w-full">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input type="email" name="" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Email" required="">
+            </div>
+        </div>
+        <button type="submit" class="inline-flex items-center px-1 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+        Add product
+        </button>
+    </body>
 </body>
 
 </html>
