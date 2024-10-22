2606fb19998f3fefe619ba63017630bd0345cb6a
A-Samaw <543231184@smktelkom-bjb.sch.id>
2024-10-22 14:06:32 +0800

    dashboard admin-user(coba)

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="login.css"/>
+<script src="https://cdn.tailwindcss.com"></script>
+</head> 
+  
+<body class="bg-white">
+   <div class="header">
+   <div class='w-full border rounded-xl  mx-auto bg-gradient-to-r from-rose-400 from-10% via-rose-350 to-pink-200'>
+   </div>
+    
+   <br>
+   <div class="inventaris">
+    <button type="button" class="w-32 rounded-xl text-white bg-rose-400">Inventaris</button>
+   </div>
+
+   <div class="peminjaman">
+    <button type="button" class="w-32 rounded-xl text-white bg-rose-400">Peminjaman</button>
+   </div>
+
+   <div class="pengembalian">
+    <button type="button" class="w-32 rounded-xl text-white bg-rose-400">Pengembalian</button>
+   </div>
+
+   </div>
+</body>
+</html>
