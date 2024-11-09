619a87536b8da7706d028d31242d2747215fc238
Dakrezzz <daffaatharizq20@gmail.com>
2024-11-10 00:38:09 +0800

    update inventaris

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Inventaris</title>
 </head>
 
-<body>
+<body class="bg-[url('img/A.png')] bg-center bg-cover h-[90vh] w-screen">
     <center>
         <h1>Inventaris</h1>
 
@@ -24,19 +25,130 @@
         <a href="{{ route('barang.create') }}"><button>Add Data</button></a>
 
         <!-- Table for Displaying Barang Data -->
-        <table border="1">
-            <tbody>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Barang</th>
-                    <th>Kategori</th>
-                    <th>Ruangan</th>
-                    <th>Tahun Masuk</th>
-                    <th>Sumber Dana</th>
-                    <th>No Barang</th>
-                    <th>Kode Barang</th>
-                    <th>Aksi</th>
-                </tr>
+       
+
+<div class="relative overflow-x-auto">
+    <div class='w-fit center border rounded-xl px-16 mt-28 py-5 mx-auto bg-gradient-to-r from-rose-400 from-10% via-rose-350 to-pink-200'>
+    <table class="w-3/5 text-sm text-left rtl:text-right text-black dark:text-black">
+        <thead class="text-xs text-white uppercase bg-white dark:text-black">
+            <tr>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    No
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Nama Barang
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Kode Barang
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Jumlah
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Kode Area
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Tahun Masuk
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Sumber Dana
+                </th>
+                <th scope="col" class="px-6 py-3 border-b dark:border-black">
+                    Opsi
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                    1
+                </th>
+                <th>
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+                <td class="px-6 py-4">
+                    Laptop
+                </td>
+                <td class="px-6 py-4">
+                    $2999
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                    2
+                </th>
+                <th>
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+                <td class="px-6 py-4">
+                    Laptop PC
+                </td>
+                <td class="px-6 py-4">
+                    $1999
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:border-black">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                 3
+                </th>
+                 <th>
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:border-black">
+  
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                 4
+                 </th>      
+                <th>
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:border-black">
+  
+                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+                    5
+                </th>      
+                <th>
+                    Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">
+                    Black
+                </td>
+                <td class="px-6 py-4">
+                    Accessories
+                </td>
+                <td class="px-6 py-4">
+                    $99
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
                 <!-- Display Search Results or All Barang Data -->
                 @forelse ($barang as $k)
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,19 +11,19 @@
 </head> 
   
 <header>
-       <div class="h-32 bg-gradient-to-r rounded- from-rose-400 from-10% via-rose-350 to-pink-200 ">
+       <div class="h-32 bg-gradient-to-r rounded-xl- from-rose-400 from-10% via-rose-350 to-pink-200 ">
       <div class="flex justify-between items-center">
-          <img class="h-auto max-w-56 mt-6 ml-28" src="{{ URL('img/login_logo.jpg') }}" alt="logo">
+          <img class="h-auto max-w-48 mt-10 ml-16" src="{{ URL('img/login_logo.jpg') }}" alt="logo">
 
     <a href="{{route ('login')}}">
-         <button type="button" class="w-36 text-white mr-5 mt-8 bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+         <button type="button" class="w-36 text-white mr-5 mt-9 bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
    </a>
 
 </div>
 
  </header>
 <br>
-<body class="bg-[url('img/A.png')] bg-center bg-cover h-[85vh] w-screen">
+<body class="bg-[url('img/A.png')] bg-center bg-cover h-[90vh] w-screen">
 
     <h1 style="font-weight: bold; margin-top: 40px; text-align:center;">SISTEM INFORMASI PEMINJAMAN SARANA DAN PRASARANA</h1>
 <div class="flex justify-between my-28 ml-96 mr-96">
