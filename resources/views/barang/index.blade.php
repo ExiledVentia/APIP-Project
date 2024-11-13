119cdbb295edef90f925480c93925929df306e6e
Dakrezzz <daffaatharizq20@gmail.com>
2024-11-13 19:56:04 +0800

    almost done inventaris

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -37,14 +37,23 @@
 
         <!-- Search Form -->
         <form action="{{ route('barang.search') }}" method="GET">
-            <div>
-                <input type="search" name="query" placeholder="Cari barang..." required autocomplete="off" />
+                        
+            <form class="max-w-md mx-auto">   
+                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                </div>
+                <input type="search" name="query" placeholder="Cari barang..." class="block w-1/4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required autocomplete="off" />
                 <button type="submit">Cari</button>
-            </div>
+            
         </form>
 
         <!-- Add Data Button -->
-        <a href="{{ route('barang.create') }}"><button>Add Data</button></a>
+        <a href="{{ route('barang.create') }}"><button class="w-fit text-black bg-rose-400 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 me-2 mb-2 dark:bg-rose-400 dark:hover:bg-rose-500 focus:outline-none dark:focus:ring-blue-800">+ Tambah Barang</button></a>
 
         <!-- Table for Displaying Barang Data -->
        
