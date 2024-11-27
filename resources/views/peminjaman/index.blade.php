7041811a26d61d38550d24f212173c3b1f2457c1
Dakrezzz <daffaatharizq20@gmail.com>
2024-11-27 18:20:53 +0800

    newest peminjaman

--- resources/views/peminjaman/index.blade.php
+++ resources/views/peminjaman/index.blade.php
@@ -109,7 +109,7 @@
 
             </form>
         </div>
-        <button type="button" onclick="nextstep()">
+        <button type="button" onclick="nextstep()" id="nextStep">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right absolute right-80 top-96" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
               </svg>
@@ -151,9 +151,9 @@
     
         // Cek kondisi saat halaman dimuat
         checkFields();
-    
     });
-
+</script>
+<script>
     let currentStep = 0;
 
     function showStep(step){
@@ -179,7 +179,8 @@
             showStep(currentStep);
         }
     }
-    
+</script>
+<script>
       // Fungsi untuk menampilkan atau menyembunyikan dropdown
       function toggleDropdown() {
             var dropdown = document.getElementById("myDropdown");
